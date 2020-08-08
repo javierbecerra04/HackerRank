2a49438a0764ff4a2b6735bdee85bda01eac4c7a
Javier Becerra <becerra.javier@javeriana.edu.co>
2020-08-07 21:42:11 -0500

    The PADS Challenge Solved

--- SQlChallenges.sql
+++ SQlChallenges.sql
@@ -7,3 +7,16 @@ CASE
     WHEN A<>B AND A<>C AND B<>C THEN 'Scalene'
 END
 FROM TRIANGLES;
+
+-- The PADS Challenge
+SELECT CONCAT(b.Name,"(",LEFT(b.Occupation, 1),")")
+FROM 
+    (SELECT Name, Occupation
+    FROM OCCUPATIONS
+    ORDER BY Name) as b;
+SELECT CONCAT("There are a total of ",a.NumProf," ",LOWER(a.Occupation),"s.")
+FROM
+    (SELECT Occupation, COUNT(*) AS NumProf
+    FROM OCCUPATIONS
+    GROUP BY Occupation
+    ORDER BY NumProf ASC,Occupation ASC) as a;
