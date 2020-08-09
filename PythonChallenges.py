0f39cf7579283dcaeaf5a3f8051f688784b3db3a
Javier Becerra <becerra.javier@javeriana.edu.co>
2020-08-09 14:12:26 -0500

    Write a function solved

--- PythonChallenges.py
+++ PythonChallenges.py
@@ -21,3 +21,16 @@ if __name__ == '__main__':
     n = int(input())
     for i in range(0,n):
         print(i*i)
+
+#Write a function
+def is_leap(year):
+    leap = False
+    if (year%4==0) and (year%100!=0):
+        leap=True
+    else:
+        if (year%100==0) and (year%400==0):
+            leap=True       
+    return leap
+
+year = int(input())
+print(is_leap(year))
